3d83977bc9468fe55baeabe69e243038836a4c03
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-05 14:13:54 +0000

    write first migration to create table

---
+++ data/migrations/20191105140218_build-cars-table.js
@@ -0,0 +1,18 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("cars", table => {
+    table.increments();
+    table
+      .text("VIN", 17)
+      .unique()
+      .notNullable();
+    table.text("make", 128).notNullable();
+    table.text("model", 128).notNullable();
+    table.text("mileage", 128).notNullable();
+    table.text("transmission type", 128);
+    table.text("status", 128);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("cars");
+};
