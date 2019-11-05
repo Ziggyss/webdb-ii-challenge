602f3254e24e2e71c8487212473e7056be2400d9
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-05 13:16:34 +0000

    initial set up

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const helmet = require('helmet');
+
+
+
+// const fruitsRouter = require('../fruits/fruits-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+// server.use('/api/fruits', fruitsRouter);
+
+module.exports = server;
